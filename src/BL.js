dca1d9af01fec7e9c27cb6981f050cf047b46e53
QTree-hallel <hallel@qtree.co.il>
2023-04-08 20:38:33 +0300

    removed default 'en' from response

--- src/BL.js
+++ src/BL.js
@@ -5,16 +5,15 @@ async function getClaims(text, targetLang = 'he') {
 
     // create API clients
     let gpt = new gptClient();
-    let translate = new translateClient()
+    let translate = new translateClient();
 
     // query GPT
     let gpt_response = await gpt.query_gpt(text);
 
     // console.log(`GPT Response: ${JSON.stringify(gpt_response)}`)
-
+    
     // generate response
     let getResponse = async () => ({
-            'en': gpt_response,
             [targetLang]: {
                 'claims': await Promise.all(
                     gpt_response['claims'].map(claim => translate.translate_text(targetLang, claim))
@@ -27,7 +26,6 @@ async function getClaims(text, targetLang = 'he') {
     console.log(a);
     return a;
 }
-
 
 module.exports = {
     getClaims
